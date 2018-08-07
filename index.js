11c9617b76cdadbdf8e759a38f788ce3706488d4
Unknown <roosverwolf@live.nl>
2018-08-07 19:03:37 +0200

    8th

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ server.post('/get-movie-details', (req, res) => {
         });
         responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
-            let dataToSend = movie === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
             return res.json({
                 fulfillmentText: dataToSend,
-             //   displayText: dataToSend,
+                displayText: dataToSend,
                 source: 'get-movie-details'
             });
         });
     }, (error) => {
         return res.json({
             fulfillmentText: 'Something went wrong!',
-          //  displayText: 'Something went wrong!',
+            displayText: 'Something went wrong!',
             source: 'get-movie-details'
         });
     });
